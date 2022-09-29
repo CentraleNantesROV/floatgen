d9c4c892abba5a3251d5c5d4a933171949baaad8
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2022-09-29 15:55:22 +0200

    adapt to sl.gz_launch

--- launch/world_launch.py
+++ launch/world_launch.py
@@ -1,17 +1,16 @@
 from simple_launch import SimpleLauncher
-import sys
 
 def generate_launch_description():
     
     sl = SimpleLauncher()
     
     sl.declare_arg('gui', default_value=True)
-    
+
     with sl.group(if_arg='gui'):
-        sl.include('ros_ign_gazebo','ign_gazebo.launch.py',launch_arguments={'ign_args': sl.name_join('-r ', sl.find('floatgen', 'floatgen_world.sdf'))})
+        sl.gz_launch(f"-r {sl.find('floatgen', 'floatgen_world.sdf')}")
         
     with sl.group(unless_arg='gui'):
-        sl.include('ros_ign_gazebo','ign_gazebo.launch.py',launch_arguments={'ign_args': sl.name_join('-s -r ', sl.find('floatgen', 'floatgen_world.sdf'))})
+        sl.gz_launch(f"-s -r {sl.find('floatgen', 'floatgen_world.sdf')}")
         
     sl.create_gz_clock_bridge()
         
