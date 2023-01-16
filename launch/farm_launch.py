5f1fcf71d4979aa2ff333a87effc19730ef3fe53
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2023-01-16 14:21:32 +0100

    add underwater collision

--- launch/farm_launch.py
+++ launch/farm_launch.py
@@ -1,12 +1,12 @@
 from simple_launch import SimpleLauncher, GazeboBridge
-import sys
+
 
 def generate_launch_description():
     
     sl = SimpleLauncher(use_sim_time = True)
     
     sl.declare_arg('nx', default_value = 1, description = 'how many turbines in X-axis')
-    sl.declare_arg('ny', default_value = 1, description = 'how many turbines in Y-axis')    
+    sl.declare_arg('ny', default_value = 1, description = 'how many turbines in Y-axis')
     
     sl.declare_arg('x', default_value = 0., description = 'X-position of first turbine')
     sl.declare_arg('y', default_value = 0., description = 'Y-position of first turbine')
@@ -18,18 +18,18 @@ def generate_launch_description():
                 
     with sl.group(ns=ns):
         
-        description = sl.robot_description('floatgen', 'farm.xacro', 
+        description = sl.robot_description('floatgen', 'farm.xacro',
                                            xacro_args=sl.arg_map(('x', 'y', 'yaw', 'nx', 'ny', 'scale', 'velocity')))
         
         # run RSP on URDF
-        sl.node('robot_state_publisher', 'robot_state_publisher', parameters={'robot_description': description})    
+        sl.node('robot_state_publisher', 'robot_state_publisher', parameters={'robot_description': description})
         
         # spawn in gazebo
         sl.spawn_gz_model(ns, only_new = False)
         
         # joint_state bridge
         gz_js_topic = sl.name_join(GazeboBridge.model_prefix(ns),'/joint_state')
-        js_bridge = GazeboBridge(gz_js_topic, 'joint_states', 'sensor_msgs/JointState', GazeboBridge.gz2ros)        
+        js_bridge = GazeboBridge(gz_js_topic, 'joint_states', 'sensor_msgs/JointState', GazeboBridge.gz2ros)
         sl.create_gz_bridge(js_bridge, 'turbine_bridge')
     
     return sl.launch_description()
