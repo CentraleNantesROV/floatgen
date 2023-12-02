a4dc592237fe695e6a4a5356ea57ebc6a8355eb5
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2023-12-02 12:49:19 +0100

    remove dummy files

--- launch/farm_launch.py
+++ launch/farm_launch.py
@@ -13,36 +13,22 @@ def generate_launch_description():
     sl.declare_arg('yaw', default_value = 0., description = 'Yaw of turbines')
     sl.declare_arg('scale', default_value = 200., description = 'Distances of turbines')
     sl.declare_arg('velocity', default_value = -2., description = 'Velocity of turbines')
-
-    sl.declare_arg('zero', False)
     
     ns = 'farm'
                 
     with sl.group(ns=ns):
+
+        # run RSP with given parameters
+        sl.robot_state_publisher('floatgen', 'farm.xacro',
+                                           xacro_args=sl.arg_map('x', 'y', 'yaw', 'nx', 'ny', 'scale', 'velocity'))
         
-        description = sl.robot_description('floatgen', 'farm.xacro',
-                                           xacro_args=sl.arg_map(('x', 'y', 'yaw', 'nx', 'ny', 'scale', 'velocity')))
-        
-        # run RSP on URDF
-        sl.node('robot_state_publisher', 'robot_state_publisher', parameters={'robot_description': description})
-        
-        # spawn in gazebo
-        with sl.group(unless_arg='zero'):
+        # spawn in Gazebo
 
-            sl.spawn_gz_model(ns, only_new = False)
+        sl.spawn_gz_model(ns, only_new = False)
 
-            # joint_state bridge
-            gz_js_topic = sl.name_join(GazeboBridge.model_prefix(ns),'/joint_state')
-            js_bridge = GazeboBridge(gz_js_topic, 'joint_states', 'sensor_msgs/JointState', GazeboBridge.gz2ros)
-            sl.create_gz_bridge(js_bridge, 'turbine_bridge')
+        # joint_state bridge
+        gz_js_topic = GazeboBridge.model_prefix(ns)+'/joint_state'
+        js_bridge = GazeboBridge(gz_js_topic, 'joint_states', 'sensor_msgs/JointState', GazeboBridge.gz2ros)
+        sl.create_gz_bridge(js_bridge, 'turbine_bridge')
 
-        with sl.group(if_arg='zero'):
-            sl.node('tf2_ros', 'static_transform_publisher',
-                    arguments=['--frame-id','turbine_1_1/pole',
-                               '--child-frame-id','turbine_1_1/blades',
-                               '--x','0.71',
-                               '--z','16.864'])
-
-
-    
     return sl.launch_description()
